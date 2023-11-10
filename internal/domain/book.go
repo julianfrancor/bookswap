2a17c0989c24ab488a2e46e146b2e1e439d7e1dd
Julian <julianfrancor@gmail.com>
2023-11-09 19:15:02 -0500

    [FTR] Add docs for Book

--- internal/domain/book.go
+++ internal/domain/book.go
@@ -6,10 +6,27 @@ var ErrBookNotFound = errors.New("book not found")
 
 // Book represents a book entity.
 type Book struct {
-	ID     int    `json:"id"`
-	UserID int    `json:"user_id"`
-	Title  string `json:"title"`
+	// ID is the unique identifier for the book.
+	// Example: 1
+	ID int `json:"id"`
+
+	// UserID is the ID of the user associated with the book.
+	// Example: 1
+	UserID int `json:"user_id"`
+
+	// Title is the title of the book.
+	// Example: "The Catcher in the Rye"
+	Title string `json:"title"`
+
+	// Author is the author of the book.
+	// Example: "J.D. Salinger"
 	Author string `json:"author"`
-	Genre  string `json:"genre"`
+
+	// Genre is the genre of the book.
+	// Example: "Fiction"
+	Genre string `json:"genre"`
+
+	// Status represents the availability status of the book.
+	// Example: "Available"
 	Status string `json:"status"`
 }
